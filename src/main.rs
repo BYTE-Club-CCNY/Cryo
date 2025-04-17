91090eef2c6a7c0f6749b963653a0eb280b8a5f4
Al Naheyan <officialk2000@gmail.com>
2025-04-17 19:02:49 -0400

    upload command done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+mod commands;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(name = "ffsend", version = "0.1", author = "al", about = "A CLI for file sharing using ffsend")]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[clap(alias = "u", alias = "up")]
+    Upload(commands::upload::UploadArgs),
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("Cargo initialized!!")
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Upload(args) => {
+            commands::upload::upload_file_cmd(args);
+        }
+    }
 }
