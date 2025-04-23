c50e0589a89ed9c0c849388c01404c5af568afd9
Al Naheyan <officialk2000@gmail.com>
2025-04-23 15:11:34 -0400

    file uploaded: download.rs

---
+++ src/commands/download.rs
@@ -0,0 +1,56 @@
+use clap::Args;
+use ffsend_api::{
+    action::download::Download,
+    api::Version,
+    client::ClientConfigBuilder,
+    file::remote_file::RemoteFile,
+};
+use ffsend_api::action::metadata::{Metadata as MetadataAction, MetadataResponse};
+use url::Url;
+use std::path::PathBuf;
+
+#[derive(Args)]
+pub struct DownloadArgs {
+    pub url: String,
+}
+
+/// download subcommand logiic
+pub fn download_file_cmd(args: DownloadArgs) {
+    match download_file(args.url) {
+        Ok(file_path) => {
+            println!("Downloaded to: {}", file_path.display());
+        }
+        Err(e) => {
+            eprintln!("Download failed: {}", e);
+        }
+    }
+}
+
+fn download_file(url: String) -> Result<PathBuf, Box<dyn std::error::Error>> {
+    let client_config = ClientConfigBuilder::default().build()?;
+    let client = client_config.client(true);
+    let version = Version::V3;
+
+    let url = Url::parse(&url)?;
+    let file = RemoteFile::parse_url(url.clone(), None)?;
+
+    // fetch metadata to get the original file name
+    let metadata_response: MetadataResponse = MetadataAction::new(&file, None, true)
+        .invoke(&client)?;
+    let file_name = metadata_response.metadata().name();
+    let target_path: PathBuf = std::env::current_dir()?.join(file_name);
+
+    // build download action with the fetched metadata to avoid refetching
+    let download = Download::new(
+        version,
+        &file,
+        target_path.clone(),
+        // TODO: password might be an issue if a user has uploaded w password. Current action doesn't account of it. 
+        None, 
+        true,
+        Some(metadata_response),
+    );
+
+    download.invoke(&client, None)?;
+    Ok(target_path)
+}
